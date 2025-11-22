6e5aa1c5b55f573a55c274871bee7418225053f6
LeatuyrBertyk <phangialoc07@gmail.com>
2025-11-22 17:55:05 +0700

    Load Mnist from binary to numpy

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
+# Khai báo thư viện
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-import gzip
 
-def load_mnist(path,kind='train'):
-	labels_path=os.path.join(path,'%s-labels.idx1-ubyte' % kind)
-	images_path=os.path.join(path,'%s-images.idx3-ubyte' % kind)
+# Đọc các file dữ liệu Mnist ở dạng binary sang numpy
+def loadMnist(path,kind='train'):
+	labelsPath=os.path.join(path,'%s-labels.idx1-ubyte' % kind)
+	imagesPath=os.path.join(path,'%s-images.idx3-ubyte' % kind)
 
-	with  open(labels_path,'rb') as lbpath:
+	with open(labelsPath,'rb') as lbpath:
 		lbpath.read(8)
 		buffer=lbpath.read()
 		labels=np.frombuffer(buffer,dtype=np.uint8)
-	with open(images_path,'rb') as imgpath:
+	with open(imagesPath,'rb') as imgpath:
 		imgpath.read(16)
 		buffer=imgpath.read()
 		images=np.frombuffer(buffer,dtype=np.uint8).reshape(len(labels),28,28).astype(np.float64)
 	return images,labels
 
-X_train, y_train=load_mnist('mnistData/', kind='train')
-print('Row: %d, columns: %d % (X_train.shape[0],X_train.shape[1])')
-fig, ax =plt.subplots(nrows=2,ncols=5,sharex=True,sharey=True,)
-ax=ax.flatten()
-for i in range(10):
-	img=X_train[y_train==i][0]
-	ax[i].imshow(img,cmap='Greys',interpolation='nearest')
+# Kiểm tra xem dữ liệu đã được chuẩn hóa chưa
 
-ax[0].set_xticks([])
-ax[0].set_yticks([])
-plt.tight_layout()
-plt.show()
+
+# Code xuất sang đồ thị của thầy (tìm trong dữ liệu đã chuẩn hóa hình đầu tiên mỗi kí tự)
+# xTrain, yTrain=loadMnist('mnistData/', kind='train')
+# print('Row: %d, columns: %d % (xTrain.shape[0],XTrain.shape[1])')
+# fig, ax =plt.subplots(nrows=2,ncols=5,sharex=True,sharey=True,)
+# ax=ax.flatten()
+# for i in range(10):
+# 	img=xTrain[yTrain==i][0]
+# 	ax[i].imshow(img,cmap='Greys',interpolation='nearest')
+
+# ax[0].set_xticks([])
+# ax[0].set_yticks([])
+# plt.tight_layout()
+# plt.show()
+
+# In ra kích thước các file để kiểm tra xem dữ liệu đã được chuẩn hóa chưa
+dataFolder = 'data' 
+
+print("Đang đọc dữ liệu...")
+trainImgs, trainLabels = loadMnist(dataFolder, kind='train')
+testImgs, testLabels = loadMnist(dataFolder, kind='t10k')
+
+print(f"1. trainImgs shape:   {trainImgs.shape}")   # Mong đợi: (60000, 28, 28)
+print(f"2. trainLabels shape: {trainLabels.shape}") # Mong đợi: (60000,)
+print(f"3. testImgs shape:    {testImgs.shape}")    # Mong đợi: (10000, 28, 28)
+print(f"4. testLabels shape:  {testLabels.shape}")  # Mong đợi: (10000,)
+
+print("Đã chuẩn bị xong 4 tập dữ liệu!")
