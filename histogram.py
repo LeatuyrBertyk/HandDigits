d89e7893068a0734589bdd48dd28b151a15a2719
luan207-max <luanbb207@gmail.com>
2025-11-25 00:00:37 +0700

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,25 +1,37 @@
-from 
+from loadMnist import loadMnist
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
 import numpy as np
 
-# tải dữ liệu MNIST
-mnist = fetch_openml('mnist_784', version=1)
-X = mnist.data.reshape(-1, 28, 28)
-y = mnist.target.astype(int)
+# 1. Load dữ liệu MNIST từ file binary
+X_train, y_train = loadMnist("mnist", kind='train')
+X_test, y_test   = loadMnist("mnist", kind='t10k')
 
-# trích HOG cho từng ảnh
-hog_features = []
-for img in X:
-    feat = hog(img, pixels_per_cell=(8,8), cells_per_block=(2,2), orientations=9)
-    hog_features.append(feat)
+# 2. Extract HOG cho training images
+hog_train = []
+for img in X_train:
+    feat = hog(img,
+               pixels_per_cell=(8, 8),
+               cells_per_block=(2, 2),
+               orientations=9)
+    hog_train.append(feat)
 
-hog_features = np.array(hog_features)
+hog_train = np.array(hog_train)
 
-# chia tập
-X_train, X_test, y_train, y_test = train_test_split(hog_features, y, test_size=0.2)
+# 3. Extract HOG cho test images
+hog_test = []
+for img in X_test:
+    feat = hog(img,
+               pixels_per_cell=(8, 8),
+               cells_per_block=(2, 2),
+               orientations=9)
+    hog_test.append(feat)
 
-# train SVM
+hog_test = np.array(hog_test)
+
+# 4. Train SVM classifier
 clf = LinearSVC()
-clf.fit(X_train, y_train)
+clf.fit(hog_train, y_train)
 
-# accuracy
-print("Accuracy:", clf.score(X_test, y_test))
+# 5. Accuracy
+print("Accuracy:", clf.score(hog_test, y_test))
