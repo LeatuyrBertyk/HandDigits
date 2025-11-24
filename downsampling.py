f91212f861bcfd966d02085ae36267b4740421fb
Ngo Nguyen <rawcorn001002@gmail.com>
2025-11-24 17:46:21 +0700

    feat: thêm chức năng downsampling để rút đặc trưng

---
+++ downsampling.py
@@ -0,0 +1,44 @@
+from loadMnist import loadMnist 
+import numpy as np
+
+# Load 4 mảng numpy (bắt buộc)
+dataFolder = 'data' 
+trainImgs, trainLabels = loadMnist(dataFolder, kind='train')
+testImgs, testLabels = loadMnist(dataFolder, kind='t10k')
+
+# Hàm downsampe_image
+def  downsample_image(img, block_size = 4):
+    h, w = img.shape # Lấy height(h) và width(w) của img, h = 28, w = 28
+    new_h = h // block_size # heigh mới = 4
+    new_w = w // block_size # width mới = 4
+    small_img = np.zeros((new_h, new_w))  # mảng để lưu ảnh sau khi downsampling
+
+    # Vòng lặp để tạo ra chuyển ma trận 28 * 28 về 7 * 7
+    for i in range(new_h):
+        for j in range(new_w):
+            block = img[i * block_size: (i + 1) * block_size, 
+                        j * block_size: (j + 1) * block_size] # lấy block 4 * 4
+            small_img[i, j] = np.mean(block) # tạo ra pixel mới 
+        
+    # Chuẩn hóa về [0,1]
+    small_img = small_img / 255.0
+    # Chuyển về nhị phân (0/1)
+    binary_img = (small_img > 0). astype(int)
+    return binary_img.flatten()
+
+# Áp dụng downsampling cho toàn bộ dataset
+trainFeatures = np.array([downsample_image(img) for img in trainImgs])
+testFeatures = np.array([downsample_image(img) for img in testImgs])
+
+# In kết quả
+
+print("Train features shape:", trainFeatures.shape)
+print("Test feature shape:", testFeatures.shape)
+
+#Lưu kết quả để dùng cho các bước tiếp theo
+np.save("train_downsampling.npy", trainFeatures)
+np.save("test_downsampling.npy", testFeatures)
+
+
+
+
