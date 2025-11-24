b7a776ddb8f846ae567fae0b8d99a6c6836bfa9b
luan207-max <luanbb207@gmail.com>
2025-11-24 23:42:53 +0700

    Update loadMnist.py

--- loadMnist.py
+++ loadMnist.py
@@ -1,17 +1,37 @@
-import os
+from loadMnist import loadMnist
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
 import numpy as np
 
-# Hàm dọc dữ liệu Mnist từ dạng binary sang mảng numpy 
-def loadMnist(path,kind='train'):
-	labelsPath=os.path.join(path,'%s-labels.idx1-ubyte' % kind)
-	imagesPath=os.path.join(path,'%s-images.idx3-ubyte' % kind)
+# 1. Load dữ liệu MNIST từ file binary
+X_train, y_train = loadMnist("mnist", kind='train')
+X_test, y_test   = loadMnist("mnist", kind='t10k')
 
-	with open(labelsPath,'rb') as lbpath:
-		lbpath.read(8)
-		buffer=lbpath.read()
-		labels=np.frombuffer(buffer,dtype=np.uint8)
-	with open(imagesPath,'rb') as imgpath:
-		imgpath.read(16)
-		buffer=imgpath.read()
-		images=np.frombuffer(buffer,dtype=np.uint8).reshape(len(labels),28,28).astype(np.float64)
-	return images,labels
+# 2. Extract HOG cho training images
+hog_train = []
+for img in X_train:
+    feat = hog(img,
+               pixels_per_cell=(8, 8),
+               cells_per_block=(2, 2),
+               orientations=9)
+    hog_train.append(feat)
+
+hog_train = np.array(hog_train)
+
+# 3. Extract HOG cho test images
+hog_test = []
+for img in X_test:
+    feat = hog(img,
+               pixels_per_cell=(8, 8),
+               cells_per_block=(2, 2),
+               orientations=9)
+    hog_test.append(feat)
+
+hog_test = np.array(hog_test)
+
+# 4. Train SVM classifier
+clf = LinearSVC()
+clf.fit(hog_train, y_train)
+
+# 5. Accuracy
+print("Accuracy:", clf.score(hog_test, y_test))
