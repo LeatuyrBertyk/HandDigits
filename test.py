a1285400010c885a68cf0efbc3f67671f1284941
LeatuyrBertyk <phangialoc07@gmail.com>
2025-11-24 15:40:47 +0700

    add comments

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ dataFolder = 'data'
 trainImgs, trainLabels = loadMnist(dataFolder, kind='train')
 testImgs, testLabels = loadMnist(dataFolder, kind='t10k')
 
-# Kiểm tra xem các hàm có đọc đúng chưa
+# Kiểm tra xem các hàm có đọc đúng chưa (chỉ là bước kiểm thử)
 print(f"1. trainImgs shape:   {trainImgs.shape}")   # Mong đợi: (60000, 28, 28)
 print(f"2. trainLabels shape: {trainLabels.shape}") # Mong đợi: (60000,)
 print(f"3. testImgs shape:    {testImgs.shape}")    # Mong đợi: (10000, 28, 28)
