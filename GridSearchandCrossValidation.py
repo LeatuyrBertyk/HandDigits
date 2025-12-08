4f0b4a52c7bac12890e2bef2f595a30129c23dc2
ncquocthang <ncquocthang@gmail.com>
2025-12-08 20:18:11 +0700

    Update GridSearchandCrossValidation.py

--- GridSearchandCrossValidation.py
+++ GridSearchandCrossValidation.py
@@ -6,4 +6,73 @@
 # được rút bằng phương pháp vectorize thì Cross Validation cho ra độ chính xác là 1
 # Cross Validation là một phương pháp đánh giá độ tốt của mô hình bằng cách chia tập test thành nhiều phần khác nhau
 # rồi luân phiên dùng mỗi phần để kiểm tra các phần còn lại để huấn luyện
-# Ví dụ: chi tập test thành 5 phần khác nhau (1,2,3,4,5) sau đó lần lượt cho phần 1 làm phần kiểm tra, (2,3,4,5) làm phần huấn luyện
+# Ví dụ: chi tập test thành 5 phần khác nhau (1,2,3,4,5) sau đó lần lượt cho phần 1 làm phần kiểm tra (2,3,4,5) làm phần huấn luyện
+# Cross-Validation chỉ áp dụng lênh tập train
+import numpy as np
+from scipy.spatial.distance import cdist  # Hỗ trợ hàm tính khoảng cách
+from scipy import stats
+
+from featureExtract.loadMnist import loadMnist 
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from featureExtract.histogram import histogramExtract 
+from featureExtract.another import anotherExtract 
+from featureExtract.downsampling import downsamplingExtract 
+
+def EuclideanDistance(x1,x2) : 
+    return np.sqrt(np.sum((x1-x2) ** 2))
+
+def KNNPredictSingle(Vectors,TestVector, LabelVectors, Kval) : 
+    distances = [EuclideanDistance(TestVector,Vector) for Vector in Vectors]
+    Kindex = np.argsort(distances)[:Kval] 
+    Knearestlabels = LabelVectors[Kindex]
+
+    most_common = stats.mode(Knearestlabels)
+    return most_common.mode[0]
+
+def KNNPredict(Vectors, TestVectors, LabelVectors, Kval) : 
+    Predictions = [KNNPredictSingle(Vectors,TestVector, LabelVectors, Kval) for TestVector in TestVectors] 
+    return np.array(Predictions) 
+
+def CalculateAccuraryScore(PredictionLabels, TrueLabels) : 
+    return np.mean(PredictionLabels == TrueLabels) 
+
+def CrossValidation(TrainningVectors, LabelVectors, Kval, cv = 5, rdstate = 42) -> float: 
+    kf = KFold(n_splits = cv, shuffle = True, random_state = rdstate) 
+    scores = [] 
+    for TrainID, ValID in kf.split(TrainningVectors) : 
+        TrainFold, ValFold = TrainningVectors[TrainID] , TrainningVectors[ValID] 
+        LabelTrainFold, LabelValFold = LabelVectors[TrainID], LabelVectors[ValID] 
+        PredictionLabels = KNNPredict(TrainFold,ValFold,LabelTrainFold, Kval) 
+        score = CalculateAccuraryScore(PredictionLabels, LabelValFold) 
+        scores.append(score) 
+    return np.mean(scores) 
+
+
+def GridSearch(FeatureVectors, LabelVectors, Kvalues) : 
+    BestScore = -np.inf 
+    BestIndex = [] 
+    for Kval in Kvalues: 
+        MeanScore = CrossValidation(FeatureVectors,LabelVectors,Kval)
+        if MeanScore > BestScore : 
+            BestScore = MeanScore
+            BestIndex = Kval 
+    return BestScore, BestIndex
+
+dataFolder = 'data'
+trainImgs, trainLabels = loadMnist(dataFolder, kind='train')
+testImgs, testLabels = loadMnist(dataFolder, kind='t10k')
+
+
+# Load các mảng bằng 4 phương pháp rút đặc trưng
+trainHistogram = np.array(histogramExtract(trainImgs)) 
+trainDownsampling = np.array([downsamplingExtract(img) for img in trainImgs])
+trainAnother = np.array(anotherExtract(trainImgs)) 
+
+Kvalues = [1,3,5,7,11,13,17,19,23,29,31,37] 
+HistogramBestScore, HistogramBestIndex = GridSearch(trainHistogram,trainLabels,Kvalues)
+DownsamplingBestScore, DownsamplingBestIndex = GridSearch(trainDownsampling,trainLabels, Kvalues)
+AnotherBestScore, AnotherBestIndex = GridSearch(trainAnother,trainLabels,Kvalues)
+print(HistogramBestScore, HistogramBestIndex) 
+print(DownsamplingBestScore,DownsamplingBestIndex) 
+print(AnotherBestScore,AnotherBestIndex) 
