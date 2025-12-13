801a33bfd75659079aa556da4487471441b324d6
LeatuyrBertyk <phangialoc07@gmail.com>
2025-12-13 15:40:53 +0700

    remove cdist in knn

--- knnClassifier.py
+++ knnClassifier.py
@@ -1,12 +1,16 @@
 import numpy as np
-from scipy.spatial.distance import cdist  # Hỗ trợ hàm tính khoảng cách
 import os
+import time
 
 from featureExtract.loadMnist import loadMnist 
 from featureExtract.vectorize import vectorizeExtract
 from featureExtract.histogram import histogramExtract 
 from featureExtract.downsampling import downsamplingExtract 
 from featureExtract.another import anotherExtract 
+
+def euclideanDistance(x1,x2) : 
+    return np.sqrt(np.sum((x1-x2) ** 2))
+
 
 dataFolder = 'data'
 trainImgs, trainLabels = loadMnist(dataFolder, kind='train')
@@ -16,23 +20,23 @@ testImgs, testLabels = loadMnist(dataFolder, kind='t10k')
 
 trainVectorize = vectorizeExtract(trainImgs)
 testVectorize = vectorizeExtract(testImgs)
-# print(f"Kích thước đặc trưng Vectorize (Train): {trainVectorize.shape}")
-# print(f"Kích thước đặc trưng Vectorize (Test): {testVectorize.shape}")
+print(f"Kích thước đặc trưng Vectorize (Train): {trainVectorize.shape}")
+print(f"Kích thước đặc trưng Vectorize (Test): {testVectorize.shape}")
 
 trainHistogram = histogramExtract(trainImgs)
 testHistogram = histogramExtract(testImgs)
-# print(f"Kích thước đặc trưng Histogram (Train): {trainHistogram.shape}")
-# print(f"Kích thước đặc trưng Histogram (Test ): {testHistogram.shape}\n")
+print(f"Kích thước đặc trưng Histogram (Train): {trainHistogram.shape}")
+print(f"Kích thước đặc trưng Histogram (Test ): {testHistogram.shape}\n")
 
 trainDownsampling = np.array([downsamplingExtract(img) for img in trainImgs])
 testDownsampling = np.array([downsamplingExtract(img) for img in testImgs])
-# print(f"Kích thước đặc trưng Downsampling (Train): {trainDownsampling.shape}")
-# print(f"Kích thước đặc trưng Downsampling (Test ): {testDownsampling.shape}\n")
+print(f"Kích thước đặc trưng Downsampling (Train): {trainDownsampling.shape}")
+print(f"Kích thước đặc trưng Downsampling (Test ): {testDownsampling.shape}\n")
 
 trainAnother = anotherExtract(trainImgs)
 testAnother = anotherExtract(testImgs)
-# print(f"Kích thước đặc trưng Hist. Intensity (Train): {trainAnother.shape}")
-# print(f"Kích thước đặc trưng Hist. Intensity (Test ): {testAnother.shape}\n")
+print(f"Kích thước đặc trưng Hist. Intensity (Train): {trainAnother.shape}")
+print(f"Kích thước đặc trưng Hist. Intensity (Test ): {testAnother.shape}\n")
  
 
 
@@ -57,33 +61,24 @@ def calculateDistances(featureMethod: str, testVectorIndex: int) -> np.ndarray:
     if testVectorIndex< 0 or testVectorIndex>= test.shape[0]:
         raise IndexError(f"{testVectorIndex} nằm ngoài phạm vi của tập test (0 đến {test.shape[0] - 1}).")
 
-    # Lấy vector truy vấn (từ tập test)
-    queryVector = test[testVectorIndex].reshape(1, -1) # Kích thước (1, D)
+    queryVector = test[testVectorIndex].flatten()
+    
+    N_train = train.shape[0] # Lấy số lượng mẫu huấn luyện
+    distances = np.zeros(N_train) 
+    for i in range(N_train):
+        distances[i] = euclideanDistance(queryVector, train[i].flatten())
 
-    # Tính khoảng cách Euclidean từ queryVector đến tất cả các vector trong train
-    # Sử dụng cdist để tối ưu hóa việc tính toán ma trận khoảng cách
-    # Kết quả distMatrix có kích thước (1, N)
-    distMatrix = cdist(queryVector, train, metric='euclidean')
-
-    # Trả về mảng khoảng cách (loại bỏ chiều đơn, kích thước N)
-    return distMatrix.flatten()
+    return distances
 
 # Cách sử dụng:
 # Downsampling: calculateDistances('downsampling',index) 
 
 
 def predictLabel(featureMethod, testIndex, k):
-    # Lấy khoảng cách
     distances = calculateDistances(featureMethod, testIndex)
-
-    # Lấy k chỉ số nhỏ nhất
     k_idx = np.argsort(distances)[:k]
-
-    # Lấy nhãn tương ứng
     y_train = featureData[featureMethod]['y_train']
     k_labels = y_train[k_idx]
-
-    # Majority vote
     labels, counts = np.unique(k_labels, return_counts=True)
     majority_label = labels[np.argmax(counts)]
 
@@ -102,12 +97,17 @@ for featureMethod, data in featureData.items():
     N_test = data['test'].shape[0]
     assignedLabels = np.zeros(N_test, dtype=int)
     
-    print(f"\n⚡️ Đang xử lý phương pháp: **{featureMethod}** (Số lượng mẫu: {N_test})")
-    
+    print(f"\n --- Đang xử lý phương pháp: **{featureMethod}** (Số lượng mẫu: {N_test})")
+   
+    startTime = time.time()
     # Gán nhãn
     for i in range(N_test):
         assignedLabels[i] = predictLabel(featureMethod, i, k=kValue)
-        
+    
+    endTime = time.time()
+    elapsedTime = endTime-startTime
+    print(f"Thời gian chạy: {elapsedTime:.2f} giây\n")
+
     output_filename = f"{featureMethod}_k{kValue}_labels.npy"
     output_filepath = os.path.join(output_dir, output_filename)
     
