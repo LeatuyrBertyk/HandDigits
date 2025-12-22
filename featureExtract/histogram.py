fcbc3e4a8734786d31555dcf282ebf285a983f53
luan207-max <luanbb207@gmail.com>
2025-12-23 00:11:59 +0700

    Update histogram.py

--- featureExtract/histogram.py
+++ featureExtract/histogram.py
@@ -2,29 +2,25 @@ try:
     from featureExtract.loadMnist import loadMnist
 except ImportError:
     from loadMnist import loadMnist
-
 import numpy as np
 import os
+from skimage.feature import hog
 
-def histogramExtract(imgs):
-    """
-    imgs: (N, 28, 28), pixel 0-255
-    return: (N, 256) histogram đã chuẩn hóa
-    """
-    n, h, w = imgs.shape
-    features = np.zeros((n, 256), dtype=float)
-
+def histogramExtract(floatImgs):
+    n,m,p = floatImgs.shape
+    featureList = []
     for i in range(n):
-        img = imgs[i]
-        hist = np.zeros(256)
-
-        for pixel in img.flatten():
-            hist[int(pixel)] += 1
-
-        hist = hist / (h * w)
-        features[i] = hist
-
-    return features
+        img = floatImgs[i]
+        hogVector = hog(img,
+                        pixels_per_cell=(8, 8),
+                        cells_per_block=(2, 2),
+                        orientations=9)
+        featureList.append(hogVector)
+    featureArray = np.array(featureList) 
+    newLen = featureArray.shape[1] // 4
+    featureArray = featureArray[:, :newLen * 4]
+    featureArray = featureArray.reshape(n, newLen, 4).mean(axis=2)
+    return featureArray.astype(float)
 
 try:
     dataFolder = 'data'
@@ -37,15 +33,16 @@ testImgs,  testLabels  = loadMnist(dataFolder, kind='t10k')
 processedTrainImgs = histogramExtract(trainImgs)
 processedTestImgs  = histogramExtract(testImgs)
 
+# print("Kích thước đặc trưng Train:", processedTrainImgs.shape)
+# print("Kích thước đặc trưng Test :", processedTestImgs.shape)
+
 try:
     OUTPUT_DIR = os.path.join('resultFeatureExtract')
 except:
-    OUTPUT_DIR = os.path.join('..', 'resultFeatureExtract')
-
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+    OUTPUT_DIR = os.path.join('..', 'resultFeatureExtract') 
 
 train_save_path = os.path.join(OUTPUT_DIR, "trainHistogram.npy")
-test_save_path  = os.path.join(OUTPUT_DIR, "testHistogram.npy")
+test_save_path = os.path.join(OUTPUT_DIR, "testHistogram.npy")
 
 np.save(train_save_path, processedTrainImgs)
 np.save(test_save_path, processedTestImgs)
