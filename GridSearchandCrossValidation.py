8f473ec390d255570265dc03cd56d1e16cca113f
ncquocthang <ncquocthang@gmail.com>
2025-12-23 07:33:20 +0700

    Update ...

--- GridSearchandCrossValidation.py
+++ GridSearchandCrossValidation.py
@@ -8,6 +8,7 @@
 # rồi luân phiên dùng mỗi phần để kiểm tra các phần còn lại để huấn luyện
 # Ví dụ: chi tập test thành 5 phần khác nhau (1,2,3,4,5) sau đó lần lượt cho phần 1 làm phần kiểm tra (2,3,4,5) làm phần huấn luyện
 # Cross-Validation chỉ áp dụng lênh tập train
+# Output Another: best accuracy: 0.3229, best K: 37
 import numpy as np
 from scipy.spatial.distance import cdist  # Hỗ trợ hàm tính khoảng cách
 from scipy import stats
@@ -20,21 +21,36 @@ from featureExtract.histogram import histogramExtract
 from featureExtract.another import anotherExtract 
 from featureExtract.downsampling import downsamplingExtract 
 
-def EuclideanDistance(x1,x2) : 
-    return np.sqrt(np.sum((x1-x2) ** 2))
-# Dự đoán nhãn cho một vector đặc trưng
 
 # Mô hình KNN
-def KNNPredictSingle(Vectors,TestVector, LabelVectors, Kval) : 
-    distances = [EuclideanDistance(TestVector,Vector) for Vector in Vectors]
-    Kindex = np.argsort(distances)[:Kval] 
-    Knearestlabels = LabelVectors[Kindex]
 
-    most_common = stats.mode(Knearestlabels)
-    return np.squeeze(most_common.mode)
+def PredictLabel(new_vector, train_vectors, train_labels, k):
+    """
+    Dự đoán nhãn bằng KNN sử dụng cdist để tối ưu tốc độ.
+    
+    Tham số:
+    - new_vector: Vector cần dự đoán (1D array-like).
+    - train_vectors: Tập hợp các vector huấn luyện (2D array-like).
+    - train_labels: Tập hợp các nhãn tương ứng (1D array-like).
+    - k: Số lượng láng giềng.
+    """
+    query = np.atleast_2d(new_vector)
+    train_x = np.asarray(train_vectors)
+    train_y = np.asarray(train_labels)
+    
+    distances = cdist(query, train_x, metric='euclidean')[0]
+    
+    k_indices = np.argpartition(distances, k)[:k]
+    
+    k_nearest_labels = train_y[k_indices]
+    
+    unique_labels, counts = np.unique(k_nearest_labels, return_counts=True)
+    prediction = unique_labels[np.argmax(counts)]
+    
+    return prediction
 
 def KNNPredict(Vectors, TestVectors, LabelVectors, Kval) : 
-    Predictions = [KNNPredictSingle(Vectors,TestVector, LabelVectors, Kval) for TestVector in TestVectors] 
+    Predictions = [PredictLabel(TestVector,Vectors, LabelVectors, Kval) for TestVector in TestVectors] 
     return np.array(Predictions) 
 
 #Cross Validation
@@ -74,12 +90,15 @@ trainHistogram = np.array(histogramExtract(trainImgs))
 trainDownsampling = np.array([downsamplingExtract(img) for img in trainImgs])
 trainAnother = np.array(anotherExtract(trainImgs)) 
 
-Kvalues = [1,3,5] 
-VectorizeBestScore, VectorizeBestIndex = GridSearch(trainVectorize,trainLabels,Kvalues) 
-print(VectorizeBestScore, VectorizeBestIndex) 
-HistogramBestScore, HistogramBestIndex = GridSearch(trainHistogram,trainLabels,Kvalues)
-print(HistogramBestScore, HistogramBestIndex) 
-DownsamplingBestScore, DownsamplingBestIndex = GridSearch(trainDownsampling,trainLabels, Kvalues)
-print(DownsamplingBestScore,DownsamplingBestIndex) 
+Kvalues =  [7,13,29,37,47,59,79,83,97,103,111]
 AnotherBestScore, AnotherBestIndex = GridSearch(trainAnother,trainLabels,Kvalues)
 print(AnotherBestScore,AnotherBestIndex) 
+Kvalues = [5,7,9,11,13,15,17,19,21,23] 
+VectorizeBestScore, VectorizeBestIndex = GridSearch(trainVectorize,trainLabels,Kvalues) 
+print(VectorizeBestScore, VectorizeBestIndex)
+Kvalues =  [5,7,9,11,13,15,17,19,21,23]
+HistogramBestScore, HistogramBestIndex = GridSearch(trainHistogram,trainLabels,Kvalues)
+print(HistogramBestScore, HistogramBestIndex)
+Kvalues =   [5,7,9,11,13,15,17,19,21,23]
+DownsamplingBestScore, DownsamplingBestIndex = GridSearch(trainDownsampling,trainLabels, Kvalues)
+print(DownsamplingBestScore,DownsamplingBestIndex) 
